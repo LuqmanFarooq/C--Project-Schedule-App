0f73d611a183f8248d4839297d0307281e6483a0
Muhammad Luqman <g00353385@gmit.ie>
2018-11-02 23:52:12 +0000

    Login Desin for Android added

---
+++ Job-Scheduler/Job-Scheduler.Android/MainActivity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Firebase;
+using Firebase.Auth;
+
+namespace Job_Scheduler.Droid
+{
+    [Activity(Label = "Job_Scheduler", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        Button btnlogin;
+        EditText input_email, input_password;
+        TextView btnSignUp, btnForgotPassword;
+        RelativeLayout main;
+
+        public static FirebaseApp jobScheduler;
+        FirebaseAuth auth;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.main);
+            // initialise firebase
+            InitFirebaseAuth();
+
+            // view
+            btnlogin = FindViewById<Button>(Resource.Id.login_btn_login);
+            input_email = FindViewById<EditText>(Resource.Id.login_email);
+            input_password = FindViewById<EditText>(Resource.Id.login_password);
+            btnSignUp = FindViewById<TextView>(Resource.Id.login_btn_sign_up);
+            btnForgotPassword = FindViewById<TextView>(Resource.Id.login_btn_forgot_password);
+
+        }
+
+        private void InitFirebaseAuth()
+        {
+            var options = new FirebaseOptions.Builder()
+           .SetApplicationId("1:475218936925:android:6fa80ae367c1bd5a")
+           .SetApiKey("AIzaSyD0nxjUglD9RJSvg0GH3_cIwN5JG3dlJaA")
+           .Build();
+
+            if (jobScheduler == null)
+                jobScheduler = FirebaseApp.InitializeApp(this, options);
+            auth = FirebaseAuth.GetInstance(jobScheduler);
+        }
+    }
+}
---
+++ Job-Scheduler/Job-Scheduler/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Job_Scheduler
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            bool isUserEmpty, isPwdEmpty;
+            isUserEmpty = String.IsNullOrEmpty(entUser.Text);
+            isPwdEmpty = String.IsNullOrEmpty(entPassword.Text);
+
+            if (isUserEmpty || isPwdEmpty)
+            {
+                // message to user not to be awkward
+            }
+            else
+            {
+                msg.Text = "you have successfully logged in";
+                // check user/pwd hash keys etc
+                // navigate to new page
+                // use navigation service to go to ZodiacPage
+                //Navigation.PushAsync(new ZodiacSigns());
+            } // end if(isUserEmpty || isPwdEmpty)
+
+
+        } //end login clicked
+
+    } // end partial class
+}
