1caafa8bcfbcbb6dd57eafafe050d6ec1fdbc41b
Muhammad Luqman <g00353385@gmit.ie>
2018-12-19 13:21:02 +0000

    fixed some bugs and finalized the app

--- Job_Scheduler/Job_Scheduler/DeleteSchedule.xaml.cs
+++ Job_Scheduler/Job_Scheduler/DeleteSchedule.xaml.cs
@@ -20,6 +20,8 @@ namespace Job_Scheduler
         public DeleteSchedule ()
 		{
             InitializeComponent();
+            // search bar initialization
+            InitSearchBar();
             // getting database connection using platform specific interface
             conn = DependencyService.Get<ISQLite>().GetConnection();
             // creating table in the database
@@ -27,6 +29,27 @@ namespace Job_Scheduler
             // getting data from database
             var data = (from add in conn.Table<Add_Schedule>() select add);
             delDatalist.ItemsSource = data;
+        }
+        // searchbar functionality
+        public void InitSearchBar()
+        {
+            dataSearch.TextChanged += (s, e) => FilterItem(dataSearch.Text);
+            dataSearch.SearchButtonPressed += (s, e) => FilterItem(dataSearch.Text);
+        }
+        // applying filters
+        public void FilterItem(string filter)
+        {
+            var data = (from add in conn.Table<Add_Schedule>() select add);
+            delDatalist.BeginRefresh();
+            if (string.IsNullOrWhiteSpace(filter))
+            {
+                delDatalist.ItemsSource = data;
+            }
+            else
+            {
+                delDatalist.ItemsSource = data.Where(x => x.name.ToLower().Contains(filter.ToLower()));
+            }
+            delDatalist.EndRefresh();
         }
         // when delete button is clicked
         private void delBtn_Clicked(object sender, EventArgs e)
@@ -37,7 +60,7 @@ namespace Job_Scheduler
             if (isIdEmpty)
             {
                 DisplayAlert("Error", "ID cannot be empty", "Try Again");
-            }
+            }// outer if
             else
             {
                 // if not empty then checking if user has enter a valid id in num or enter something else
@@ -45,7 +68,7 @@ namespace Job_Scheduler
                 if (!int.TryParse(delId.Text, out num))
                 {
                     CrossToastPopUp.Current.ShowToastMessage("enter valid id");
-                }
+                }// inner if
                 else
                 {
                     // if valid id is entered
@@ -55,6 +78,9 @@ namespace Job_Scheduler
                     conn.Table<Add_Schedule>().Delete(x => x.id == entId);
                     // message if deleted
                     CrossToastPopUp.Current.ShowToastMessage("Record Deleted Successfully");
+
+                    // ---------------------refreshing the page------------------------------------------
+
                     InitializeComponent();
                     // getting database connection using platform specific interface
                     conn = DependencyService.Get<ISQLite>().GetConnection();
@@ -63,8 +89,10 @@ namespace Job_Scheduler
                     // getting data from database
                     var data = (from add in conn.Table<Add_Schedule>() select add);
                     delDatalist.ItemsSource = data;
-                }
-            }
+
+                    // ---------------------refreshing the page------------------------------------------
+                }// inner else
+            }// outer else
         }
     }
 }
